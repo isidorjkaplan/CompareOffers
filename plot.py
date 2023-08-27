2b8593bb05c6d59118a353c1a3cfb2c4ac3a04e7
Isidor Kaplan <isidorjkaplan@mail.utoronto.ca>
2023-08-26 20:59:15 -0400

    Updating plots with more stuff

--- plot.py
+++ plot.py
@@ -18,51 +18,35 @@ def plot_values(values, title='plot', names = None, quarterly = True):
     plt.title(title)
     pass
 
-def plot_quarterly(results : List[Result]):
-    noffers = len(results)
-    labels = ["%s (%s)" % (result.offer.label, result.city.label) for result in results]
-    
-    plt.subplot(211)
-    plot_values([result.net_worth for result in results], title='Net Worth',names=labels)
-    plt.ylabel("$1,000 USD")
-
-    #plot_values([np.cumsum(headlands), np.cumsum(hrt)], title='Total Cumulative Profits',names=['Headlands', 'HRT'])
-    
-    plt.subplot(212)
-    plot_values([result.savings_cashflow for result in results], title='Net Savings (quarterly)')
-    plt.xlabel("Years")
-
-    plt.show()
-    pass
-
 def annualize(arr, agg_func = np.sum) -> list:
     return [agg_func(arr[i:(i+4)]) for i in range(0, len(arr), 4)]
 
-def plot_yearly(results : List[Result]):
+def plot_results(results : List[Result]):
     noffers = len(results)
     labels = ["%s (%s)" % (result.offer.label, result.city.label) for result in results]
     
-    plt.subplot(221)
-    plot_values([annualize(result.net_worth, lambda arr: arr[0]) for result in results], title='Net Worth (Start Of Year)',names=labels, quarterly=False)
-    plt.ylabel("Hundred Thousand USD")
+    plt.subplot(231)
+    plot_values([result.net_worth for result in results], title='Net Worth',names=labels)
+    plt.ylabel("$1,000")
+    plt.xlabel("Year")
+
+    plt.subplot(232)
+    plot_values([result.savings_cashflow for result in results], title='Net Savings (quarterly)', quarterly=False)
+    plt.xlabel("Quarter")
 
     #plot_values([np.cumsum(headlands), np.cumsum(hrt)], title='Total Cumulative Profits',names=['Headlands', 'HRT'])
     
-    plt.subplot(222)
+    plt.subplot(234)
     plot_values([annualize(result.raw_cashflow) for result in results], title='Raw TC (yearly)', quarterly=False)
+    plt.xlabel("Year")
 
-    plt.subplot(223)
+    plt.subplot(235)
     plot_values([annualize(result.taxed_cashflow) for result in results], title='After-Tax TC (yearly)', quarterly=False)
+    plt.xlabel("Year")
 
-    plt.subplot(224)
+    plt.subplot(236)
     plot_values([annualize(result.savings_cashflow) for result in results], title='Net Savings (yearly)', quarterly=False)
-    
-    plt.xlabel("Calender Year Number")
+    plt.xlabel("Year")
 
 
     plt.show()
-
-def plot_results(results : List[Result]):
-    plot_quarterly(results)
-    plot_yearly(results)
-    pass
